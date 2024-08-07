e13a43bb6479b29481ac3444346ab91120eaa9e6
Kennet Mathisen <kennetam02@gmail.com>
2024-08-07 19:57:22 +0200

    mutiply by deltat ime

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 use macroquad::prelude::*;
 use macroquad::ui::root_ui;
 
-const SCREEN_WIDTH: u16 = 1920;
-const SCREEN_HEIGHT: u16 = 1080;
-
 struct Player {
     pos: Vec3,
     phi: f64,
@@ -74,9 +71,9 @@ impl Enviorment {
 fn window_conf() -> Conf {
     Conf {
         window_title: "test".to_owned(),
-        window_width: SCREEN_WIDTH as i32,
-        window_height: SCREEN_HEIGHT as i32,
-        window_resizable: false,
+        window_width: 1920,
+        window_height: 1080,
+        // window_resizable: false,
         ..Default::default()
     }
 }
@@ -114,28 +111,30 @@ async fn main() {
             std::process::exit(1);
         }
     };
-
+    
     loop {
+        let scale_height = (screen_height() / 2.0) as f64;
+
         clear_background(env.sky_color);
 
-        render(&mut p, 300.0, 500.0, &env);
+        render(&mut p, scale_height, 500.0, &env, screen_width() as f64, screen_height() as f64);
 
         draw_text(&format!("{} FPS", get_fps()), 10.0, 20.0, 30.0, BLACK);
         draw_text(&format!("xyz: {}, {}, {}", p.pos.x.round(), p.pos.y.round(), p.pos.z.round()), 100.0, 20.0, 30.0, BLACK);
         
         for v in get_keys_down() {
             match v {
-                KeyCode::A => p.phi += 0.1,
-                KeyCode::D => p.phi -= 0.1,
+                KeyCode::A => p.phi += (5.0  * get_frame_time()) as f64,
+                KeyCode::D => p.phi -= (5.0 * get_frame_time()) as f64,
 
-                KeyCode::Q => p.horizon += 10.0,
-                KeyCode::E => p.horizon -= 10.0,
+                KeyCode::Q => p.horizon += (500.0 * get_frame_time()) as f64,
+                KeyCode::E => p.horizon -= (500.0 * get_frame_time()) as f64,
 
-                KeyCode::S => p.move_in_dir(10.0),
-                KeyCode::W => p.move_in_dir(-10.0),
+                KeyCode::S => p.move_in_dir(500.0 * get_frame_time()),
+                KeyCode::W => p.move_in_dir(-500.0 * get_frame_time()),
 
-                KeyCode::Space => p.pos.z += 5.0,
-                KeyCode::LeftShift => p.pos.z -= 5.0,
+                KeyCode::Space => p.pos.z += 500.0 * get_frame_time(),
+                KeyCode::LeftShift => p.pos.z -= 500.0 * get_frame_time(),
                 _ => (),
             }
         }
@@ -144,12 +143,12 @@ async fn main() {
     }
 }
 
-fn render(p: &mut Player, scale_height: f64, distance: f64, env: &Enviorment) {
+fn render(p: &mut Player, scale_height: f64, distance: f64, env: &Enviorment, screen_width: f64, screen_height: f64) {
     let sinphi = p.phi.sin();
     let cosphi = p.phi.cos();
 
-    let mut ybuffer = [SCREEN_HEIGHT as f64; SCREEN_WIDTH as usize];
-
+    let mut ybuffer = vec![screen_height; screen_width as usize];
+    
     let mut dz = 1.0;
     let mut z = 1.0;
 
@@ -160,10 +159,10 @@ fn render(p: &mut Player, scale_height: f64, distance: f64, env: &Enviorment) {
         let p_right_x = (cosphi * z - sinphi * z) + p.pos.x as f64;
         let p_right_y = (-sinphi * z - cosphi * z) + p.pos.y as f64;
 
-        let dx = (p_right_x - p_left_x) / SCREEN_WIDTH as f64;
-        let dy = (p_right_y - p_left_y) / SCREEN_WIDTH as f64;
+        let dx = (p_right_x - p_left_x) / screen_width;
+        let dy = (p_right_y - p_left_y) / screen_width;
 
-        for i in 0..SCREEN_WIDTH {
+        for i in 0..screen_width as usize {
             let wrapped_x = p_left_x.abs() as u32 % 1024;
             let wrapped_y = p_left_y.abs() as u32 % 1024;
 
@@ -179,9 +178,9 @@ fn render(p: &mut Player, scale_height: f64, distance: f64, env: &Enviorment) {
 
                 let color: Color = lerp_color(source_color, env.fog_color, t);
                 draw_line(
-                    i.into(),
+                    i as f32,
                     height_on_screen as f32,
-                    i.into(),
+                    i as f32,
                     ybuffer[i as usize] as f32,
                     1.0,
                     color,
